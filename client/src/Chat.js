af3345fb071162d9b46e86edcff2b5e8085487a1
Prathamesh <it.a.34.prathamesh.govilkar@gmail.com>
2023-10-16 19:31:06 +0530

    chat.js

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,73 +1,84 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import ScrollToBottom from "react-scroll-to-bottom";
-import "./Chat.css"
+import "./Chat.css";
 
-function Chat({username, room, socket}){
-    const [input, setInput] = useState("");
-    const [messageList, setMessageList] = useState([]);
+function Chat({ username, room, socket }) {
+  const [input, setInput] = useState("");
+  const [messageList, setMessageList] = useState([]);
+  const [isTyping, setIsTyping] = useState(false);
 
-    const  sendIt = async () =>{
-        if(input !== ""){
-         const MessageData = {
-            author: username,
-            room: room,
-            message: input,
-         };
-         
-         await socket.emit("send_message", MessageData);
-         setMessageList((list) => [...list, MessageData]);
-         setInput("");
-         console.log(MessageData)
-       }
-    };
-    useEffect(() => {
-        socket.on("receive", (data) =>{
-            console.log(data);
-            setMessageList((list) => [...list, data]);    
-        });
-    }, [socket]);
+  const sendIt = async () => {
+    if (input !== "") {
+      const MessageData = {
+        author: username,
+        room: room,
+        message: input,
+        timestamp: new Date().toLocaleTimeString(),
+      };
 
+      await socket.emit("send_message", MessageData);
+      setMessageList((list) => [...list, MessageData]);
+      setInput("");
+      console.log(MessageData);
+    }
+  }
 
-    return(
-        <>
-        <div className="main">
-            <div className="header">
-                <h3>HELLO FROM ADI</h3>
+  useEffect(() => {
+    socket.on("receive", (data) => {
+      console.log(data);
+      setMessageList((list) => [...list, data]);
+    });
+
+    socket.on("user_typing", (user) => {
+      setIsTyping(true);
+    });
+
+    socket.on("user_stopped_typing", (user) => {
+      setIsTyping(false);
+    });
+  }, [socket]);
+
+  return (
+    <div className="main">
+      <div className="header">
+        <h3>HELLO FROM {username === "" ? 'No one is in the chat' : username}</h3>
+      </div>
+      <div className="body_message">
+        <ScrollToBottom className='message_container'>
+          {messageList.map((messageContent, index) => (
+            <div className="message" key={index} id={username === messageContent.author ? "you" : "other"}>
+              <div>
+                <p>{messageContent.message}</p>
+                <div className="timestamp">{messageContent.timestamp}</div>
+              </div>
             </div>
-            <div className="body_message">
-            <ScrollToBottom className='message_container'>
-                 {messageList.map((messageContent, index) => {
-                     return(
-                         <>
-                         
-                         <div className="message" key={index} id={username === messageContent.author ? "you" : "other" }>
-                            <p>{messageContent.message}</p>
-                            <div className="author">
-                                {username === messageContent.author ? "you" : messageContent.author}                             
-                            </div>
-                         </div>
-                        
-                        </>
-                     )
-                 })}
-             </ScrollToBottom>
+          ))}
+          {isTyping && (
+            <div className="typing-indicator">
+              <p>User is typing...</p>
             </div>
-        </div>
-            <div className="footer">
-                <input className="inp" type="text" placeholder="...."
-                value={input}
-                onChange={(event) =>{
-                 setInput(event.target.value);
-                }}
-                onKeyPress={(e) => {
-                    e.key === "Enter" && sendIt();
-                }} />
-                <button className="btn" onClick={sendIt}>&#9658;</button>
-            </div>
-            
-        
-        </>
-    )
+          )}
+        </ScrollToBottom>
+      </div>
+      <div className="footer">
+        <input
+          className="inp"
+          type="text"
+          placeholder="Say something..."
+          value={input}
+          onChange={(event) => {
+            setInput(event.target.value);
+            socket.emit("user_typing");
+          }}
+          onKeyPress={(e) => {
+            e.key === "Enter" && sendIt();
+            socket.emit("user_stopped_typing");
+          }}
+        />
+        <button className="btn" onClick={sendIt}>&#9658;</button>
+      </div>
+    </div>
+  );
 }
 
 export default Chat;
