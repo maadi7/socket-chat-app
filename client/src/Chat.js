d3af6c21969e28305c397a0c9f4bb3f6e09862a5
ayushjadhavvv <ayushjadhav292@gmail.com>
2023-10-17 17:23:32 +0530

    modified Chat.js

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -5,79 +5,78 @@ import "./Chat.css";
 function Chat({ username, room, socket }) {
   const [input, setInput] = useState("");
   const [messageList, setMessageList] = useState([]);
-  const [isTyping, setIsTyping] = useState(false);
 
   const sendIt = async () => {
     if (input !== "") {
-      const MessageData = {
+      const messageData = {
         author: username,
         room: room,
         message: input,
-        timestamp: new Date().toLocaleTimeString(),
+        timestamp: new Date().toISOString(), // Add a timestamp to the message
       };
 
-      await socket.emit("send_message", MessageData);
-      setMessageList((list) => [...list, MessageData]);
+      await socket.emit("send_message", messageData);
+      setMessageList((list) => [...list, messageData]);
       setInput("");
-      console.log(MessageData);
+      console.log(messageData);
     }
-  }
+  };
 
   useEffect(() => {
     socket.on("receive", (data) => {
       console.log(data);
       setMessageList((list) => [...list, data]);
     });
-
-    socket.on("user_typing", (user) => {
-      setIsTyping(true);
-    });
-
-    socket.on("user_stopped_typing", (user) => {
-      setIsTyping(false);
-    });
   }, [socket]);
 
   return (
-    <div className="main">
-      <div className="header">
-        <h3>HELLO FROM {username === "" ? 'No one is in the chat' : username}</h3>
-      </div>
-      <div className="body_message">
-        <ScrollToBottom className='message_container'>
-          {messageList.map((messageContent, index) => (
-            <div className="message" key={index} id={username === messageContent.author ? "you" : "other"}>
-              <div>
-                <p>{messageContent.message}</p>
-                <div className="timestamp">{messageContent.timestamp}</div>
-              </div>
-            </div>
-          ))}
-          {isTyping && (
-            <div className="typing-indicator">
-              <p>User is typing...</p>
-            </div>
-          )}
-        </ScrollToBottom>
+    <>
+      <div className="main">
+        <div className="header">
+          <h3>
+            HELLO FROM {username === "" ? "No one is in the chat" : username}
+          </h3>
+        </div>
+        <div className="body_message">
+          <ScrollToBottom className="message_container">
+            {messageList.map((messageContent, index) => {
+              return (
+                <div
+                  className="message"
+                  key={index}
+                  id={username === messageContent.author ? "you" : "other"}
+                >
+                  <p>{messageContent.message}</p>
+                  <div className="author">
+                    {username === messageContent.author
+                      ? "you"
+                      : messageContent.author}{" "}
+                    ({new Date(messageContent.timestamp).toLocaleTimeString()})
+                  </div>
+                </div>
+              );
+            })}
+          </ScrollToBottom>
+        </div>
       </div>
       <div className="footer">
         <input
           className="inp"
           type="text"
-          placeholder="Say something..."
+          placeholder="Say something...."
           value={input}
           onChange={(event) => {
             setInput(event.target.value);
-            socket.emit("user_typing");
           }}
           onKeyPress={(e) => {
             e.key === "Enter" && sendIt();
-            socket.emit("user_stopped_typing");
           }}
         />
-        <button className="btn" onClick={sendIt}>&#9658;</button>
+        <button className="btn" onClick={sendIt}>
+          &#9658;
+        </button>
       </div>
-    </div>
+    </>
   );
 }
 
